caea331f61c2ed4458277ad9974907e9749721ae
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-28 20:08:04 +0000

    connected to mongoDB in the server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -6,12 +7,18 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: true }));
 
-const init = () => {
+const init = async () => {
   try {
-    app.listen(PORT, () => {
-      console.log(`navigate to http://localhost:${PORT}`);
+    await mongoose.connect("mongodb://localhost:27017/socialNetworkDb", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-  } catch (error) {}
+    console.log("[INFO] : Successfully connected to db");
+
+    app.listen(PORT, () => console.log(`[INFO] : server running on ${PORT}`));
+  } catch (error) {
+    console.log(`[ERROR] :  Connection to db has failed | ${error.message}`);
+  }
 };
 
 init();
