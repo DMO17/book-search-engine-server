ef85c38692a1131389d9f430b07eb6ab8f49bb6e
Dahir Mohamed <dmoabdi98@gmail.com>
2022-03-17 19:15:00 +0000

    removed console

--- src/resolvers/searchBooks.js
+++ src/resolvers/searchBooks.js
@@ -4,7 +4,6 @@ const { getBookData } = require("../helper/util");
 const searchBooks = async (_, { searchTerm }) => {
   try {
     const books = await getBookData(searchTerm);
-    console.log(searchTerm);
     return books;
   } catch (error) {
     console.log(`[ERROR]: Failed to search for books | ${error.message}`);
